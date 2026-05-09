64f2cdcfafe5ce34fab696a005e5f776a215d866
DracFiendMG <sreeramrdy@gmail.com>
2026-05-09 23:48:32 +0530

    refactor: update layout and Home component to improve responsiveness and structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${nunitoSans.className}`}
+        className={`${nunitoSans.className} max-w-[1440px] mx-auto`}
       >
         <Header />
         {children}
--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,12 @@ export default function Home() {
       <h1 className={`text-[54px]/[100%] font-bold text-[#1E1E1E] ${lato.className}`}>Find your next favourite book</h1>
       <p className="mt-5 mb-10 text-[23px]">A cosy corner of the web where readers discover hand‑picked titles across every genre, from timeless classics to hidden indie gems.</p>
       <button className="text-xl font-semibold uppercase border-[2px] border-black-900 p-3">Browse Books</button>
-      <Image style={imageStyle} src="/hero-image-home.png" width={465} height={465} alt="Beach Image" />
+      <div className="md:hidden">
+        <Image style={imageStyle} src="/hero-image-home.png" width={465} height={465} alt="Beach Image" />
+      </div>
+      <div className="hidden md:block">
+        <Image src="/hero-image.png" width={465} height={465} alt="Beach Image" />
+      </div>
       {/* <div className="fixed w-50 h-50 bg-white rounded-[50%] top-1/2 left-1/2 transform -translate-x-1/2 translate-y-1/2"></div> */}
     </div>
   );
