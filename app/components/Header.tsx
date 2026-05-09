6c552a67439c1c0d115c2f39c5ed88c36895237a
DracFiendMG <sreeramrdy@gmail.com>
2026-05-09 20:42:49 +0530

    feat: implement Header component with navigation links; update Home page layout and content

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,3 +1,21 @@
+import Image from "next/image"
+import Link from "next/link"
+
 export default function Header() {
-    return <h1>This is my Header</h1>
+    return (
+        <header className="flex justify-between items-center px-5">
+            <Link href="/">
+                <Image 
+                    src="/tinylibrary-logo.png"
+                    width={80}
+                    height={53}
+                    alt="Tiny Library Logo"
+                />
+            </Link>
+            <ul className="flex gap-10 text-sm uppercase">
+                <li><Link href="/books">Books</Link></li>
+                <li><Link href="/about">About</Link></li>
+            </ul>
+        </header>
+    )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+import { Lato } from "next/font/google";
+
+const lato = Lato({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["300", "400", "700", "900"]
+})
+
 export default function Home() {
   return (
-    <div>
-      <p>This is my home page</p>
+    <div className="p-5">
+      <h1 className={`text-[54px]/[100%] font-bold text-[#1E1E1E] ${lato.className}`}>Find your next favourite book</h1>
+      <p>A cosy corner of the web where readers discover hand‑picked titles across every genre, from timeless classics to hidden indie gems.</p>
+      <button>Browse Books</button>
+      <Image src="/hero-image-square.png" width={465} height={465} alt="Beach Image" />
     </div>
   );
 }
