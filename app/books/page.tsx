afb817555477eedb55833507e52cebb9c34f63cb
DracFiendMG <sreeramrdy@gmail.com>
2026-05-10 16:55:17 +0530

    feat: implement Books page with book listing and details; add getAllBooks function

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,3 +1,25 @@
-export default function Books() {
-    return <h1>This is Books page</h1>
+import { FaRegHeart } from "react-icons/fa6"
+import { getAllBooks } from "../lib/books"
+import Image from "next/image"
+
+export default async function Books() {
+    const books = await getAllBooks()
+    return (
+        <main>
+            {books.map(book => {
+                return <div key={book.id}>
+                    <div className="w-[362px] h-[362px] relative">
+                        <Image src="/hero-image-square.png" fill alt={book.name} style={{ objectFit: 'cover' }} />
+                    </div>
+                    <h1>{book.name}</h1>
+                    <p>{book.author}</p>
+                    <p>{book.category}</p>
+                    <div>
+                        <FaRegHeart />
+                        <span>{book.likes}</span>
+                    </div>
+                </div>
+            })}
+        </main>
+    )
 }
---
+++ app/lib/books.ts
@@ -0,0 +1,5 @@
+import booksData from '../data/books.json'
+
+export async function getAllBooks() {
+    return booksData
+}
