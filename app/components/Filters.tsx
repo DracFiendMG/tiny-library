8232963c3256fd11dcc39a385e6e2079405679c0
DracFiendMG <sreeramrdy@gmail.com>
2026-05-11 21:09:55 +0530

    feat: add BooksCategory and BooksGrid components; implement basic structure for category display

--- app/components/Filters.tsx
+++ app/components/Filters.tsx
@@ -4,7 +4,7 @@ import { getBooksCategory } from "../lib/books"
 export default async function Filters() {
     const filters = await getBooksCategory()
     return (
-        <nav className="no-scrollbar uppercase overflow-scroll flex gap-5 text-[14px] p-5 text-[#606060] [mask-image:linear-gradient(to_right,transparent,white_5%,white_90%,transparent_100%)]">
+        <nav className="no-scrollbar uppercase overflow-scroll flex gap-5 text-[14px] p-5 text-[#606060]">
             {
                 filters.map(filter => {
                     return <Link className="whitespace-nowrap" key={filter} href={filter}>{ filter }</Link>
