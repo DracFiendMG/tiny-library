33c7e86c735a3939c480dc5523599c459ecbe430
DracFiendMG <sreeramrdy@gmail.com>
2026-05-11 22:07:20 +0530

    feat: refactor Books and BooksGrid components; implement getAllBooks function with category filtering

--- app/lib/books.ts
+++ app/lib/books.ts
@@ -1,7 +1,8 @@
 import booksData from '../data/books.json'
+import type { Book } from '../types/types'
 
-export async function getAllBooks() {
-    return booksData
+export async function getAllBooks(category: Partial<string | undefined>) {
+    return category ? booksData.filter((book: Book) => book.category === category) : booksData
 }
 
 export async function getBooksCategory() {
